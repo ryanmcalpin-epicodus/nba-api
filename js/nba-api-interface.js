e17689f88186856e1842cf6c41c19a2f296b6627
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-20 11:34:30 -0700

    add ajax request to get all teams

--- js/nba-api-interface.js
+++ js/nba-api-interface.js
@@ -2,6 +2,19 @@ var Team = require('./../js/nba-api.js').teamModule;
 
 $(document).ready(function() {
   $('#time').text(moment());
+
+  $.get({
+    type : "GET",
+    url : "https://api.fantasydata.net/v3/nba/stats/json/AllTeams",
+    beforeSend : function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', 'af76518ec911448db5176fa4a9627059');},
+    success : function(result) {
+      console.log(result);
+    },
+    error : function(result) {
+      console.log(request);
+    }
+  });
+
   $('#team-butt').click(function() {
     var team = $('#team').val();
     $('#teams').text("You've selected the " + team + ".");
