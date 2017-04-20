495353fcf721cb7d6e8c5d4af76452fdd7972e68
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-20 13:03:00 -0700

    add sass, inline styles for list items

--- js/nba-api-interface.js
+++ js/nba-api-interface.js
@@ -1,4 +1,5 @@
 var Team = require('./../js/nba-api.js').teamModule;
+var apiKey = require('./../.env').apiKey;
 
 $(document).ready(function() {
   $('#time').text(moment());
@@ -6,17 +7,23 @@ $(document).ready(function() {
   $.get({
     type : "GET",
     url : "https://api.fantasydata.net/v3/nba/stats/json/AllTeams",
-    beforeSend : function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', 'af76518ec911448db5176fa4a9627059');},
+    beforeSend : function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', apiKey);},
     success : function(result) {
       console.log(result);
+      var teams = result;
+      teams.forEach(function(team) {
+        $('#teams').append("<li style='color:#" + team.SecondaryColor +  "; background-color:#" + team.PrimaryColor + "; text-shadow: 3px 2px #" + team.TertiaryColor + ";' class='" + team.TeamID + "'>" + team.City + " " + team.Name + "</li>");
+      });
     },
     error : function(result) {
       console.log(request);
     }
   });
 
-  $('#team-butt').click(function() {
-    var team = $('#team').val();
-    $('#teams').text("You've selected the " + team + ".");
-  });
+
+
+  // $('#team-butt').click(function() {
+  //   var team = $('#team').val();
+  //   $('#teams').text("You've selected the " + team + ".");
+  // });
 });
