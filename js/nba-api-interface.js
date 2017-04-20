4f5479fb27fb9e96cbe143bd6e2c747a8ffde837
Ryan McAlpin <rhmcalpin@gmail.com>
2017-04-20 14:13:05 -0700

    refactor to separate front and back end

--- js/nba-api-interface.js
+++ js/nba-api-interface.js
@@ -1,29 +1,14 @@
-var Team = require('./../js/nba-api.js').teamModule;
-var apiKey = require('./../.env').apiKey;
+var NBA = require('./../js/nba-api.js').NBAModule;
 
 $(document).ready(function() {
-  $('#time').text(moment());
+  $('#time').text(moment().format('dddd, MMM Do, h:mm A'));
 
-  $.get({
-    type : "GET",
-    url : "https://api.fantasydata.net/v3/nba/stats/json/AllTeams",
-    beforeSend : function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', apiKey);},
-    success : function(result) {
-      console.log(result);
-      var teams = result;
-      teams.forEach(function(team) {
-        $('#teams').append("<li style='color:#" + team.SecondaryColor +  "; background-color:#" + team.PrimaryColor + "; text-shadow: 3px 2px #" + team.TertiaryColor + ";' class='" + team.TeamID + "'>" + team.City + " " + team.Name + "</li>");
-      });
-    },
-    error : function(result) {
-      console.log(request);
-    }
-  });
-
-
-
-  // $('#team-butt').click(function() {
-  //   var team = $('#team').val();
-  //   $('#teams').text("You've selected the " + team + ".");
-  // });
+  var nba = new NBA();
+  nba.getTeams(displayTeams);
 });
+
+var displayTeams = function(teams) {
+  teams.forEach(function(team) {
+    $('#teams').append("<li style='color:#" + team.PrimaryColor +  "; background-color:#" + team.SecondaryColor + "; text-shadow: 3px 2px #" + team.TertiaryColor + ";' class='" + team.TeamID + "'>" + team.City + " " + team.Name + "</li>");
+  });
+}
--- js/nba-api.js
+++ js/nba-api.js
@@ -1,7 +1,24 @@
-function Team() {
+var apiKey = require('./../.env').apiKey;
 
+function NBA() {
 }
 
+NBA.prototype.getTeams = function(displayTeams) {
+  $.get({
+    type : "GET",
+    url : "https://api.fantasydata.net/v3/nba/stats/json/AllTeams",
+    beforeSend : function(xhr){xhr.setRequestHeader('Ocp-Apim-Subscription-Key', apiKey);},
+    success : function(result) {
+      console.log(result);
+      var teams = result;
+      displayTeams(teams);
+    },
+    error : function(result) {
+      $('#teams').text(error.responseJSON.message);
+    }
+  }).then(function(response) {
 
+  });
+};
 
-exports.teamModule = Team;
+exports.NBAModule = NBA;
